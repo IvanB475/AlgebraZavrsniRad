73f89dc938ba617ced4202507d2947eaf3e571e4
Ivan <ivanbeljan@net.hr>
2020-05-13 20:32:34 +0200

    added mongoose connection

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
 app.get("/", (req, res) => {
   res.send("Hello");
 });
 
-app.listen(8000, console.log("success"));
+mongoose.connect("mongodb://localhost:27017/salarycalc", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("success");
+});
+
+app.listen(8000, console.log("server started"));
