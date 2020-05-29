9ff573b66de92c6eadb618cac738773ed6aed743
ivanbeljan <ivanbeljanofficial@gmail.com>
2020-05-29 22:59:34 +0200

    added navbar, header ejs files, added express-session, csrf, path, Closes #19

--- app.js
+++ app.js
@@ -5,12 +5,28 @@ const passport = require("passport");
 const User = require("./models/user");
 const LocalStrategy = require("passport-local").Strategy;
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const csrf = require('csurf');
+const path = require('path');
+
+
+app.use(
+  session({
+    secret: "MYSECRET",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 const usersRoutes = require("./routes/users");
 
 const usersControllers = require("./controllers/users");
 
+
+const csrfProtection = csrf();
+
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(csrfProtection);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -19,14 +35,23 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+})
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use(usersRoutes);
 app.use(usersControllers);
 
+
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.render("index/landing", { path: "index/landing"});
 });
 
 mongoose.connect("mongodb://localhost:27017/zavrsnirad", {
