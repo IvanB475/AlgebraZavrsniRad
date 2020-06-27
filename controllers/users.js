dc45fc5928397078575a4d9ed91893c966f7ed7a
Ivan <ivanbeljan@net.hr>
2020-06-27 02:05:41 +0200

    now user is able to add newsfeed from his profile and live comment/ see comments

--- controllers/users.js
+++ controllers/users.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 const passport = require("passport");
-const crypto = require('crypto');
-const nodemailer = require('nodemailer');
-const async = require('async');
+const crypto = require("crypto");
+const nodemailer = require("nodemailer");
+const async = require("async");
 const Post = require("../models/post");
-const io = require('../socket');
+const io = require("../socket");
 
 router.post("/signup", (req, res) => {
   const newUser = new User({
@@ -20,17 +20,19 @@ router.post("/signup", (req, res) => {
     } else {
       passport.authenticate("local")(req, res, () => {
         console.log("uspješna registracija");
-        res.render("index/landing", { path: "index/landing" , users: 0 });
+        res.render("index/landing", { path: "index/landing", users: 0 });
       });
     }
   });
 });
 
-router.post("/login", passport.authenticate("local", {
-  successRedirect: "/",
-  failureRedirect: "/login"
-}));
-
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+  })
+);
 
 router.post("/logout", (req, res) => {
   req.logout();
@@ -38,178 +40,219 @@ router.post("/logout", (req, res) => {
   console.log("successfully logged you out");
 });
 
-router.post('/settings', (req, res, next) => {
-  const update = { email: req.body.email, imageUrl: req.file.path}
+router.post("/settings", (req, res, next) => {
+  const update = { email: req.body.email, imageUrl: req.file.path };
   console.log(req.file);
-  User.findByIdAndUpdate(req.user._id, update).then(result => {
-    res.render('index/landing', {path: '/', users: 0});
-    console.log(result);
-  }).catch(err => {
-    const error = new Error(err);
-    error.httpStatusCode = 500;
-    return next(error);
-  })
-})
-
-router.post('/resetpw', (req,res, next) => {
-  async.waterfall([
-    function(done) {
-      crypto.randomBytes(32, (err, buf) => {
-        let token = buf.toString('hex');
-        done(err,token);
-      });
-    },
-    function(token, done) {
-      User.findOne({ email: req.body.email}, (err, user) => {
-        if(!user) {
-          console.log('error' + ' No account with that email address exists');
-          return res.redirect('/');
-        }
-
-        user.resetPasswordToken = token;
-
-        user.resetPasswordExpires = Date.now() + 3600000;
-
-        user.save(function(err) {
-          done(err, token, user);
-        });
-      });
-    },
-    (token, user, done ) => {
-      let smtpTransport = nodemailer.createTransport({
-        service: 'Gmail',
-        auth: {
-          user: 'rubotester@gmail.com',
-          pass: 'rubo54545'
-        }
-      });
-      let mailOptions = {
-        to: user.email,
-        from: 'rubotester@gmail.com',
-        subject: 'Node.js Password Reset',
-        text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
-          'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-          'http://' + req.headers.host + '/resetpw/' + token + '\n\n' +
-          'If you did not request this, please ignore this email and your password will remain unchanged.\n'
-      };
-      smtpTransport.sendMail(mailOptions, (err) => {
-        console.log('mail sent');
-        console.log('success', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
-        done(err, 'done');
-      });
-    }
-  ], (err) => {
-    if(err) return next(err);
-    res.redirect('/');
-  })
-})
-
-router.post('/resetpw/:token', (req, res) => {
-  async.waterfall([
-    function(done) {
-      User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, (err, user) => {
-        if (!user) {
-          console.log("nop");
-          return res.redirect('back');
-        }
-        if(req.body.password === req.body.confirm) {
-          user.setPassword(req.body.password, function(err) {
-            user.resetPasswordToken = undefined;
-            user.resetPasswordExpires = undefined;
-
-            user.save((err) => {
-              req.logIn(user, function(err) {
-                done(err, user);
-              });
-            });
-          })
-        } else {
-            console.log("passwords don't match");
-            return res.redirect('back');
-        }
-      });
-    },
-    function(user, done) {
-      var smtpTransport = nodemailer.createTransport({
-        service: 'Gmail', 
-        auth: {
-          user: 'rubotester@gmail.com',
-          pass: 'rubo54545'
-        }
-      });
-      var mailOptions = {
-        to: user.email,
-        from: 'rubotester@gmail.com',
-        subject: 'Your password has been changed',
-        text: 'Hello,\n\n' +
-          'This is a confirmation that the password for your account ' + user.email + ' has just been changed.\n'
-      };
-      smtpTransport.sendMail(mailOptions, function(err) {
-        console.log("password has been changed!");
-        done(err);
-      });
-    }
-  ], function(err) {
-    res.redirect('/');
-  });
+  User.findByIdAndUpdate(req.user._id, update)
+    .then((result) => {
+      res.render("index/landing", { path: "/", users: 0 });
+      console.log(result);
+    })
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
 });
 
+router.post("/resetpw", (req, res, next) => {
+  async.waterfall(
+    [
+      function (done) {
+        crypto.randomBytes(32, (err, buf) => {
+          let token = buf.toString("hex");
+          done(err, token);
+        });
+      },
+      function (token, done) {
+        User.findOne({ email: req.body.email }, (err, user) => {
+          if (!user) {
+            console.log("error" + " No account with that email address exists");
+            return res.redirect("/");
+          }
+
+          user.resetPasswordToken = token;
+
+          user.resetPasswordExpires = Date.now() + 3600000;
+
+          user.save(function (err) {
+            done(err, token, user);
+          });
+        });
+      },
+      (token, user, done) => {
+        let smtpTransport = nodemailer.createTransport({
+          service: "Gmail",
+          auth: {
+            user: "rubotester@gmail.com",
+            pass: "rubo54545",
+          },
+        });
+        let mailOptions = {
+          to: user.email,
+          from: "rubotester@gmail.com",
+          subject: "Node.js Password Reset",
+          text:
+            "You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n" +
+            "Please click on the following link, or paste this into your browser to complete the process:\n\n" +
+            "http://" +
+            req.headers.host +
+            "/resetpw/" +
+            token +
+            "\n\n" +
+            "If you did not request this, please ignore this email and your password will remain unchanged.\n",
+        };
+        smtpTransport.sendMail(mailOptions, (err) => {
+          console.log("mail sent");
+          console.log(
+            "success",
+            "An e-mail has been sent to " +
+              user.email +
+              " with further instructions."
+          );
+          done(err, "done");
+        });
+      },
+    ],
+    (err) => {
+      if (err) return next(err);
+      res.redirect("/");
+    }
+  );
+});
+
+router.post("/resetpw/:token", (req, res) => {
+  async.waterfall(
+    [
+      function (done) {
+        User.findOne(
+          {
+            resetPasswordToken: req.params.token,
+            resetPasswordExpires: { $gt: Date.now() },
+          },
+          (err, user) => {
+            if (!user) {
+              console.log("nop");
+              return res.redirect("back");
+            }
+            if (req.body.password === req.body.confirm) {
+              user.setPassword(req.body.password, function (err) {
+                user.resetPasswordToken = undefined;
+                user.resetPasswordExpires = undefined;
+
+                user.save((err) => {
+                  req.logIn(user, function (err) {
+                    done(err, user);
+                  });
+                });
+              });
+            } else {
+              console.log("passwords don't match");
+              return res.redirect("back");
+            }
+          }
+        );
+      },
+      function (user, done) {
+        var smtpTransport = nodemailer.createTransport({
+          service: "Gmail",
+          auth: {
+            user: "rubotester@gmail.com",
+            pass: "rubo54545",
+          },
+        });
+        var mailOptions = {
+          to: user.email,
+          from: "rubotester@gmail.com",
+          subject: "Your password has been changed",
+          text:
+            "Hello,\n\n" +
+            "This is a confirmation that the password for your account " +
+            user.email +
+            " has just been changed.\n",
+        };
+        smtpTransport.sendMail(mailOptions, function (err) {
+          console.log("password has been changed!");
+          done(err);
+        });
+      },
+    ],
+    function (err) {
+      res.redirect("/");
+    }
+  );
+});
 
 router.post("/sendFriendReq", (req, res) => {
   const friendId = req.body.wantedUserId;
-  User.findById(friendId).then(friend => {
-    return req.user.addToFriends(friend);
-  }).then( () => { 
-  res.render('index/landing', { users: 0, path: '/'})
-}).catch ( () => {
-  console.log("Failed");
-})
-})
+  User.findById(friendId)
+    .then((friend) => {
+      return req.user.addToFriends(friend);
+    })
+    .then(() => {
+      res.render("index/landing", { users: 0, path: "/" });
+    })
+    .catch(() => {
+      console.log("Failed");
+    });
+});
 
 router.post("/acceptFriendReq", (req, res) => {
   const friendId = req.body.acceptedFriendId;
   console.log(friendId);
   console.log(req.user._id);
-  User.findById(friendId).then(friend => {
-    return req.user.acceptFriend(friend);
-  }).then( () => { 
-  res.render('index/landing', { users: 0, path: '/'})
-}).catch ( () => {
-  console.log("Failed");
-})
+  User.findById(friendId)
+    .then((friend) => {
+      return req.user.acceptFriend(friend);
+    })
+    .then(() => {
+      res.render("index/landing", { users: 0, path: "/" });
+    })
+    .catch(() => {
+      console.log("Failed");
+    });
 });
 
-
 router.post("/room-register", (req, res) => {
-  if(req.body.room === req.body.kod){
+  if (req.body.room === req.body.kod) {
     User.findById(req.user._id, (err, user) => {
-      if(err) {
+      if (err) {
         res.redirect("/");
       } else {
         user.rooms.push(req.body.room);
         user.save();
-        res.render("index/socket", { path: 'index/socket'});
+        res.render("index/socket", { path: "index/socket" });
       }
-    })
+    });
   } else {
     res.redirect("/");
-}
-})
-
-router.post("/room" , (req, res) => {
-  res.render("index/room", {path: 'index/room', name: req.body.room})
+  }
 });
 
-router.post("/newsfeed",(req, res) => {
+router.post("/room", (req, res) => {
+  res.render("index/room", { path: "index/room", name: req.body.room });
+});
+
+router.post("/newsfeed", (req, res) => {
   const post = new Post({
     author: req.user._id,
     username: req.user.username,
-    post: req.body.newpost
+    post: req.body.newpost,
   });
   post.save();
-  io.getIO().emit('posts', { post:post , postid: post._id });
+  io.getIO().emit("posts", { post: post, postid: post._id });
   res.redirect("/newsfeed");
-})
+});
 
-
+router.post("/privatePost", (req, res) => {
+  const post = new Post({
+    author: req.user._id,
+    username: req.user.username,
+    post: req.body.newpost,
+  });
+  post.save();
+  io.getIO().emit("privatePost", { post: post, postid: post._id });
+  io.getIO().emit("posts", { post: post, postid: post._id });
+  res.redirect("/settings");
+});
 module.exports = router;
---
+++ public/socketProfile.js
@@ -0,0 +1,82 @@
+//make connection
+var socket = io.connect("http://localhost:8000");
+
+//buttons and inputs
+var postlist = $("#postlist");
+var send_message = $("#send_message");
+var username = $("#username");
+var comment = $("#comment");
+var postid = $("#postid");
+var commentlist = $(".commentlist");
+var sendcomment = $("#sendcomment");
+
+$(function () {
+  $(".selector").on("keyup click", function () {
+    commentlist = $(this).find(".commentlist");
+    comment = $(this).find("#comment");
+    sendcomment = $(this).find("#sendcomment");
+    postid = $(this).find("#postid").val();
+    comment.on("keyup", (event) => {
+      if (event.keyCode === 13) {
+        sendcomment.click();
+      }
+    });
+  });
+
+  send_message.click(function () {
+    socket.emit("privatePost");
+  });
+
+  //Listen on new_message
+  socket.on("privatePost", (data) => {
+    console.log("ušao tu");
+    console.log(data.post.postid);
+    postlist.prepend(
+      "<h5>" +
+        data.post.username +
+        "</h5> <textarea rows='5' cols='50' readonly>" +
+        data.post.post +
+        "</textarea>" +
+        `<div class="selector"> <div class="commentlist" id="commentlist${data.postid}">
+    </div>
+    </br>
+    <selection>
+        <input type="hidden" id="username" value='<%=currentUser.username %>'>
+        <input type="hidden" name="_csrf" value='<%= csrfToken %>'>
+        <input type="hidden" id="postid" value='${data.postid}'>
+        <input type="text" id='comment'>
+        <button id="sendcomment" type="submit" onclick="mySubmit()">Comment!</button> 
+    </selection>
+</div>`
+    );
+
+    $(".selector").on("keyup click", function () {
+      commentlist = $(this).find(".commentlist");
+      comment = $(this).find("#comment");
+      postid = $(this).find("#postid").val();
+    });
+  });
+
+  socket.on("comments", (data) => {
+    commentlist = "commentlist" + data.postid;
+    commentlist = $(`#${commentlist}`);
+    console.log(commentlist);
+    commentlist.append(
+      "<em>" +
+        data.comment.username +
+        "</em>" +
+        `<input type='text' class="form-control-plaintext" value= "${data.comment.message}" + readonly></br>`
+    );
+  });
+});
+
+function mySubmit() {
+  if (comment.val().length > 0) {
+    socket.emit("comments", {
+      username: username.val(),
+      comment: comment.val(),
+      postid: postid,
+    });
+    comment.val("");
+  } else return;
+}
