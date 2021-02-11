3033d18cbaf7082690cd9455398fcb3f43f37a5a
Ivan <ivanbeljan@net.hr>
2021-02-11 10:24:00 +0100

    added accept button for calls

--- public/socket.js
+++ public/socket.js
@@ -1,132 +1,146 @@
-       var username = document.getElementById("username").value; 
-       var sender = document.getElementById("sender").value;
-       var senderid = document.getElementById("senderid").value;
-       var usersocket = document.getElementById("usersocket").value;
-       var userid = document.getElementById("userid").value;
-       //make connection
-       var socket = io.connect('http://localhost:8000', {query:`user=${username}`})
+var username = document.getElementById("username").value;
+var sender = document.getElementById("sender").value;
+var senderid = document.getElementById("senderid").value;
+var usersocket = document.getElementById("usersocket").value;
+var userid = document.getElementById("userid").value;
+//make connection
+var socket = io.connect("http://localhost:8000", { query: `user=${username}` });
 
-       //buttons and inputs
-       var message = $("#message")
-       var send_message = $("#send_message")
-       var callFriend = document.getElementById("callFriend");
-       var chatroom = $("#chatroom")
-       var feedback = $("#feedback")
-       const peerConnection = new RTCPeerConnection();
-   console.log("hi sender" + senderid + "hi user" + userid);
+//buttons and inputs
+var message = $("#message");
+var send_message = $("#send_message");
+var callFriend = document.getElementById("callFriend");
+var acceptButton = document.getElementById("acceptButton");
+var chatroom = $("#chatroom");
+var feedback = $("#feedback");
+const peerConnection = new RTCPeerConnection();
+console.log("hi sender" + senderid + "hi user" + userid);
+var friendSocket;
 
-      
-   document.addEventListener("DOMContentLoaded", function(event) { 
-    console.log("workd");
-    socket.emit('privateconn', {userid: userid, senderid: senderid, roomName: makeid(8)})
+document.addEventListener("DOMContentLoaded", function (event) {
+  console.log("workd");
+  socket.emit("privateconn", {
+    userid: userid,
+    senderid: senderid,
+    roomName: makeid(8),
+  });
+});
+
+callFriend.addEventListener("click", function () {
+  callUser(usersocket);
+});
+$(function () {
+  //Emit message
+  send_message.click(function () {
+    socket.emit("private", {
+      msg: message.val(),
+      from: sender,
+      userid: userid,
+      senderid: senderid,
+      roomName: makeid(8),
+    });
+    feedback.html("");
+    message.val("");
   });
 
-  callFriend.addEventListener("click", function() {
-      callUser(usersocket);
+  //Listen on new_message
+  socket.on("private", (data) => {
+    feedback.html("");
+    chatroom.append(
+      "<p class='message'>" + data.from + " -> " + data.msg + "</p>"
+    );
   });
-   $(function(){
-       //Emit message
-       send_message.click(function(){
-           socket.emit("private", {msg: message.val(), from: sender, userid: userid, senderid: senderid, roomName: makeid(8)})
-           feedback.html('');
-           message.val('');
-       })
 
-
-       //Listen on new_message
-       socket.on("private", (data) => {
-        feedback.html('');
-           chatroom.append("<p class='message'>" + data.from + " -> " + data.msg +"</p>");
-   })
-
-   message.on('keyup', (event) => {
-    if(event.keyCode === 13) {
-        send_message.click();
+  message.on("keyup", (event) => {
+    if (event.keyCode === 13) {
+      send_message.click();
     }
-})
+  });
 
-       //Emit typing
-       message.on("keypress", () => {
-           socket.emit('typing', { from: sender })
-       })
-   
-       //Listen on typing
-       socket.on('typing', (data) => {
-           feedback.html("<p><i>" + data.from + " is typing a message..." + "</i></p>")
-       })
-   });
+  //Emit typing
+  message.on("keypress", () => {
+    socket.emit("typing", { from: sender });
+  });
 
-   async function callUser(socketId) {
-       console.log("offer sent");
-       const offer = await peerConnection.createOffer();
-       await peerConnection.setLocalDescription( new RTCSessionDescription(offer));
+  //Listen on typing
+  socket.on("typing", (data) => {
+    feedback.html(
+      "<p><i>" + data.from + " is typing a message..." + "</i></p>"
+    );
+  });
+});
 
-       socket.emit("call-user", {
-           offer,
-           to: socketId
-       })
-   }
+async function callUser(socketId) {
+  console.log("offer sent");
+  const offer = await peerConnection.createOffer();
+  await peerConnection.setLocalDescription(new RTCSessionDescription(offer));
 
-   socket.on("call-made", async data => {
-       await peerConnection.setRemoteDescription(
-           new RTCSessionDescription(data.offer)
-       );
+  socket.emit("call-user", {
+    offer,
+    to: socketId,
+  });
+}
 
-       const answer = await peerConnection.createAnswer();
-       await peerConnection.setLocalDescription(new RTCSessionDescription(answer));
+socket.on("call-made", async (data) => {
+  await peerConnection.setRemoteDescription(
+    new RTCSessionDescription(data.offer)
+  );
+  friendSocket = data.socket;
+});
 
-       socket.emit("make-answer", {
-           answer,
-           to: data.socket
-       })
-   });
+acceptButton.addEventListener("click", async () => {
+  const answer = await peerConnection.createAnswer();
+  await peerConnection.setLocalDescription(new RTCSessionDescription(answer));
 
-   socket.on("answer-made", async data => {
-       let isAlreadyCalling = false;
-       await peerConnection.setRemoteDescription(
-           new RTCSessionDescription(data.answer)
-       );
+  socket.emit("make-answer", {
+    answer,
+    to: friendSocket,
+  });
+});
 
-       if(!isAlreadyCalling) {
-           callUser(data.socket);
-           isAlreadyCalling = true;
-       }
-   });
+socket.on("answer-made", async (data) => {
+  let isAlreadyCalling = false;
+  await peerConnection.setRemoteDescription(
+    new RTCSessionDescription(data.answer)
+  );
 
+  if (!isAlreadyCalling) {
+    callUser(data.socket);
+    isAlreadyCalling = true;
+  }
+});
 
-   navigator.getUserMedia(
-       {video: true, audio: true },
-       stream => {
-           const localVideo = document.getElementById("local-video");
-           if(localVideo) {
-               localVideo.srcObject = stream;
-           }
-
-           stream.getTracks().forEach(track => peerConnection.addTrack(track, stream));
-       },
-       error => {
-           console.log(error.message);
-       }
-   );
-
-   peerConnection.ontrack = function({streams: [stream]}) {
-       const remoteVideo = document.getElementById("remote-video");
-       if(remoteVideo) {
-           remoteVideo.srcObject = stream;
-       }
-   }
-   
-
-   
-
-
-   function makeid(length) {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
-       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+navigator.getUserMedia(
+  { video: true, audio: true },
+  (stream) => {
+    const localVideo = document.getElementById("local-video");
+    if (localVideo) {
+      localVideo.srcObject = stream;
     }
-    return result;
- }
- 
+
+    stream
+      .getTracks()
+      .forEach((track) => peerConnection.addTrack(track, stream));
+  },
+  (error) => {
+    console.log(error.message);
+  }
+);
+
+peerConnection.ontrack = function ({ streams: [stream] }) {
+  const remoteVideo = document.getElementById("remote-video");
+  if (remoteVideo) {
+    remoteVideo.srcObject = stream;
+  }
+};
+
+function makeid(length) {
+  var result = "";
+  var characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
